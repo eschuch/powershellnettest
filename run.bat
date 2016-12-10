bf4ac238742e40e0f12520d5e81bb612554d3caf
Erico Lima Schuch <eschuch@gmail.com>
2016-12-10 17:29:30 -0200

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,111 @@
+@echo off
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+:: Se voce esta lendo este texto, voce deve fechar esta janela, renomear o arquivo para run.bat e executar novamente
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ECHO.*********************************************************************
+echo.* Executando testes     ...                                         *
+echo.*                                                                   *
+echo.* Os testes sao continuos. Caso queira terminar, feche esta janela. *
+echo.* E gerado um arquivo de nome:                                      *
+echo.*                                    -------         resultado.log  *
+echo.* Envie este arquivo para analise.                                  *
+ECHO.*********************************************************************
+call :sub >>resultado.log
+exit /b
+
+:sub
+echo @#@ Inicio ciclo de teste
+echo @#@ date
+date /t
+echo @#@ time
+time /t
+ECHO.**********************************************************
+echo @#@ ipconfig
+ipconfig
+ECHO.**********************************************************
+echo @#@ route print
+route print
+ECHO.**********************************************************
+echo #@# ping 8.8.8.8
+ping 8.8.8.8
+ECHO.**********************************************************
+echo @#@ tracert -d 8.8.8.8
+tracert -d 8.8.8.8
+ECHO.**********************************************************
+echo @#@ tracert -4 google.com
+tracert -4 google.com
+ECHO.**********************************************************
+echo @#@ tracert -4 terra.com.br
+tracert -4 terra.com.br
+ECHO.**********************************************************
+ECHO @#@ tracert -4 uol.com.br
+tracert -4 uol.com.br
+ECHO.**********************************************************
+echo #@# ping 2001:4860:4860::8888
+ping 2001:4860:4860::8888
+ECHO.**********************************************************
+echo @#@ tracert -d -6 2001:4860:4860::8888
+tracert -d -6 2001:4860:4860::8888
+ECHO.**********************************************************
+echo @#@ tracert -6 google.com
+tracert -6 google.com
+ECHO.**********************************************************
+echo @#@ tracert -6 terra.com.br
+tracert -6 terra.com.br
+ECHO.**********************************************************
+ECHO @#@ tracert -6 uol.com.br
+tracert -6 uol.com.br
+GOTO SUB
